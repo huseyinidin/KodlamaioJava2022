9e045f7e06f34f250cde0bda5835f558bcb3c5d9
Hüseyin idin <huseyinidinn@gmail.com>
2023-02-20 00:29:43 +0300

    CRUD - Mappers - Model Mapper Service

---
+++ week6/campSpring/rentACar/src/main/java/kodlama/io/rentACar/RentACarApplication.java
@@ -0,0 +1,20 @@
+package campSpring.rentACar.src.main.java.kodlama.io.rentACar;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class RentACarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentACarApplication.class, args);
+	}
+	
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
+
+}
